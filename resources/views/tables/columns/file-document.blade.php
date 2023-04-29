df631bd3d1a5dbf5d8abfa2f165f603d826562bf
Wender Rocha <wender_dev@hotmail.com>
2023-04-29 20:46:21 -0300

    registrando commit do terminal

--- resources/views/tables/columns/file-document.blade.php
+++ resources/views/tables/columns/file-document.blade.php
@@ -5,7 +5,7 @@
 
     @if($getState() !== null && !empty($getState()))
 
-        <a href="{{ asset("/storage/".$getState()) }}" download>
+        <a href="{{ asset("/public/storage/".$getState()) }}" download>
             <button>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                      stroke="currentColor" stroke-width="2">
